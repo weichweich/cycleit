2bd6a06d104ad8e94e299512be6444cad47503eb
Albrecht Weiche <uodnk@student.kit.edu>
2019-05-11 15:11:15 +0200

    serializers

--- server/cycleit/views.py
+++ server/cycleit/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+from cycleit.models import BycicleModel, Manufactures
+from cycleit.serializers import ManufacturesSerializer
+
+
+@csrf_exempt
+def snippet_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = Manufactures.objects.all()
+        serializer = ManufacturesSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ManufacturesSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
