7cbb7668710303530d1195ac706554f6a313f984
Albrecht Weiche <uodnk@student.kit.edu>
2019-05-11 20:31:31 +0200

    proxy

--- server/cycleit_be/urls.py
+++ server/cycleit_be/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('cycleit.urls')),
+    path('api/', include('cycleit.urls')),
     path('admin/', admin.site.urls),
 ]
